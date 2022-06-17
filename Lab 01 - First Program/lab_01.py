fcabcefb8f457c2da7c433e0b1c22dd03f4f14b2
Jake Chung <jakechung2222@gmail.com>
2022-06-17 19:34:22 -0400

    chapter 5.

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,7 @@
+# Lab 1
+#  https://www.youtube.com/watch?v=wYofTQaIIlQ
+
+import arcade
+
+arcade.open_window(600, 600, 'First Window')
+arcade.run()
---
+++ Testing/HelloWorld.py
@@ -0,0 +1,27 @@
+# One way to comment code
+# We all know very well.
+
+"""
+Another way to comment code
+using multi-line.
+These are the same.
+"""
+
+print('Hello World!')
+
+# Multiline string
+single = "Single line string"
+print(single)
+
+multi = """abc
+def
+ghi
+"""
+print(multi)
+
+## AMAZING!!
+formatted = f"""
+Single:  {single}
+Multi:  {multi}
+"""
+print(formatted)
