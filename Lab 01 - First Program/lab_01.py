a5758b591177c873f1b8103811b83a4b56fdacbf
Jake Chung <jakechung2222@gmail.com>
2022-06-17 19:53:06 -0400

    working on it.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -4,4 +4,10 @@
 import arcade
 
 arcade.open_window(600, 600, 'First Window')
+
+arcade.set_background_color(arcade.csscolor.SIENNA)
+arcade.start_render()
+arcade.draw_rectangle_filled(300, 300, 25, 25, arcade.csscolor.AZURE)
+arcade.finish_render()
+
 arcade.run()
