559f16c2a5e18dc8e1b057ee18ec98e752d06af1
Jake Chung <jakechung2222@gmail.com>
2022-06-23 19:15:46 -0400

    Fixed Lab 2 objects

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -2,8 +2,29 @@
 import arcade
 
 # Window
-arcade.open_window(500, 500, 'Lab 2 Drawing')
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, 'Lab 2 Drawing')
 arcade.set_background_color((5, 20, 50))
+
+def draw_boat(x, y):
+    arcade.draw_arc_filled(x, y, 150, 100, (100, 50, 25), 180, 360)
+    arcade.draw_rectangle_filled(x, y + 37, 10, 75, (100, 100, 100))
+    arcade.draw_polygon_filled(
+        [ (x + 5, y + 25),
+          (x + 50, y + 25),
+          (x + 5, y + 75) ] ,
+         (200, 200, 200)
+    )
+
+def draw_fish(x, y, color):
+    arcade.draw_ellipse_filled(x, y, 50, 35, color)
+    arcade.draw_triangle_filled(
+        x - 25, y,
+        x - 45, y - 25,
+        x - 45, y + 25,
+        color
+    )
 
 # Rendering
 arcade.start_render()
@@ -12,12 +33,18 @@ arcade.draw_circle_filled(250, 220, 100, arcade.csscolor.WHITE)
 arcade.draw_rectangle_filled(250, 100, 500, 250, (0, 50, 50))
 arcade.draw_polygon_filled(
     [(150, 225),
-    (100, 0),
-    (400, 0),
-    (350, 225)],
-    (200, 255, 255)
+     (100, 0),
+     (400, 0),
+     (350, 225)],
+     (200, 255, 255)
 )
 arcade.draw_arc_filled(250, 225, 200, 200, (200, 225, 255), 180, 360)
+
+# For Testing
+draw_boat(250, 250)
+draw_fish(100, 100, (100, 0, 0))
+draw_fish(200, 50, (0, 100, 0))
+
 arcade.finish_render()
 
 arcade.run()
