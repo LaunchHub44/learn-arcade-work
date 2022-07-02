c6b605ee836ebfef23d459f7bf7d7604e91792f1
Jake Chung <jakechung2222@gmail.com>
2022-07-01 20:46:42 -0400

    Saving text adventure

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,25 +1,48 @@
 import random
 
+#
+# ADVANCED Topic:
+#
+# You are welcome to use either of "case 1" or "case 2"
+#
+# case 2 is preferred.
+#  In industry, fewer variables are better!
+#
 
 class Room:
     def __init__(self,
                  room_type: str = "",
                  directions: str = "",
-                 haunted: int = random.randint(0, 3)
+                 ghost_chance: int = random.randint(0,3)
                  ):
         self.room_type: str = room_type
         self.directions: str = directions
-        self.haunted: int = haunted
+        self.ghost_chance: int = ghost_chance
 
-        if self.haunted == 3:
-            self.haunted = True
-        else:
-            self.haunted = False
+        # case 1
+        self.is_haunted_var = False
+        if ghost_chance >= 3:
+            self.is_haunted_var = True
+
+    # case 2  (in industry practice, this is preferred. )
+    def is_haunted(self) -> bool:
+        return self.ghost_chance >= 3
 
 
 def main():
     lives = 3
     dead = False
+
+    r = Room()
+    if r.is_haunted_var:
+        # We maintain state in a state variable:  is_haunted_var
+        pass
+
+    # OR,
+    if r.is_haunted():
+        # We calculate haunted status, everytime we check.
+        pass
+
 
     while not dead:
         pass
